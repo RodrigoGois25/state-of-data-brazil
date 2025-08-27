b1519bf28ed3f9f1dcb6c71ba13d8374dbfcdff2
moreuo <98001570+moreuo@users.noreply.github.com>
2025-08-27 12:34:14 -0300

    fix: remove data leak on notebook caused by smote, adjustment in the flask app and n8n workflow

--- app/app.py
+++ app/app.py
@@ -1,57 +1,45 @@
-import os
-
-import joblib
 import pandas as pd
 from flask import Flask, jsonify, render_template, request
+from joblib import load
 
 app = Flask(__name__)
 
-# Carregando os modelos
-caminho_modelo = os.path.join("..", "models", "modelo_cargo_pipeline.joblib")
-caminho_encoder = os.path.join("..", "models", "label_encoder_cargo.joblib")
-model_pipeline = joblib.load(caminho_modelo)
-label_encoder = joblib.load(caminho_encoder)
-print("Modelo e Encoder carregados com sucesso.")
+# Carregando Modelos e Encoders
+try:
+    model = load("../models/neural_network_model.joblib")
+    labels = load("../models/label_encoder.joblib")
+    print(
+        """Modelo e Encoder Carregados com Sucesso!\n
+        São Esperadas as Colunas:""",
+        model.feature_names_in_,
+    )
+except Exception as error:
+    print(error)
+    raise
 
 
-@app.route("/")
+# Rota Home
+@app.route("/", methods=["GET"])
 def home():
     return render_template("index.html")
 
 
+# Rota de Predição
 @app.route("/predict", methods=["POST"])
 def predict():
-    # Receber os dados da requisição em formato JSON
-    json_data = request.get_json()
-
-    # --- INÍCIO DA NOVA DEPURAÇÃO ---
-    print("\n--- DADOS RECEBIDOS DO N8N ---")
-    print(json_data)
-    print("-----------------------------\n")
-    # --- FIM DA NOVA DEPURAÇÃO ---
-
-    # Se não receber nenhum dado JSON, retorna um erro claro
-    if json_data is None:
-        return jsonify(
-            {"erro": "Nenhum dado JSON recebido no corpo da requisição."}
-        ), 400
-
-    # Converter os dados JSON em um DataFrame do Pandas
-    dados_usuario = pd.DataFrame(json_data, index=[0])
+    try:
+        # Converte a Requisição JSON em DataFrame
+        data = pd.DataFrame(request.get_json(), index=[0])
+        print(data)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
     try:
-        # Usar o pipeline para fazer a predição
-        predicao_numerica = model_pipeline.predict(dados_usuario)
-
-        # Converter a predição numérica de volta para o nome do cargo (texto)
-        cargo_predito = label_encoder.inverse_transform(predicao_numerica)
-
-        # Retornar o resultado como JSON
-        return jsonify({"cargo_predito": cargo_predito[0]})
-
+        numeric_prediction = model.predict(data)
+        label_prediction = labels.inverse_transform(numeric_prediction)
+        return jsonify({"predicted_job": label_prediction[0].title()})
     except Exception as e:
-        # Em caso de erro, retornar uma mensagem de erro
-        return jsonify({"erro": str(e)}), 400
+        return jsonify({"error": str(e)}), 400
 
 
 if __name__ == "__main__":
